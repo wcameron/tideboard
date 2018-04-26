847e83bdd525e8f2d0e347d4632f479fa55f3842
wcameron <walter@waltercameron.com>
2018-04-25 20:27:31 -0800

    Serve new build directory

--- server/config/express.js
+++ server/config/express.js
@@ -9,8 +9,8 @@ var config = require('./index');
 
 module.exports = function(app) {
   app.use(compression());
-  app.use(express.static(path.join(config.root, 'client')));
-  app.set('appPath', path.join(config.root, 'client')); // define the path of our app inside express to use across the server if needed
+  app.use(express.static(path.join(config.root, 'public')));
+  app.set('appPath', path.join(config.root, 'public')); // define the path of our app inside express to use across the server if needed
   app.use(morgan('dev'));
   app.use(errorHandler()); // error handler
 };
